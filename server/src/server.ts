9a0a50f8aa8d91f51723c7e9a76707039d403cfe
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-08-07 13:21:45 -0400

    API finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
+import routes from './routes';
 const port = process.env.PORT || 8080
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/users', (request, response) => {
-  return response.send('Hello world!');
-})
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`App is on ${port}`);
