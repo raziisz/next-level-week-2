f36cb6a22f1c332c23a1e1d40c486a810c84f95a
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-08-07 09:40:45 -0400

    migrations and config knex

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello world')
+import express from 'express';
+const port = process.env.PORT || 8080
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.send('Hello world!');
+})
+
+app.listen(port, () => {
+  console.log(`App is on ${port}`);
+});
