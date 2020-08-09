01057948c14e338a1f7da48cff45ca720ecb9531
Luiz Felipe <raziel_libertino@hotmail.com>
2020-08-09 13:49:05 -0400

    paginas de dar aula

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import styles from './styles';
 
+import giveClassesBgImage from '../../assets/images/give-classes-background.png'
+import { useNavigation } from '@react-navigation/native';
 const GiveClasses = () => {
-    return <View />
+    const navigation = useNavigation();
+
+    function handleNavigateBack() {
+        navigation.goBack();
+    }
+    return (
+        <View style={styles.container}>
+            <ImageBackground 
+                resizeMode="contain" 
+                source={giveClassesBgImage} 
+                style={styles.content}>
+                    <Text style={styles.title}>
+                        Quer ser um Proffy?
+                    </Text>
+                    <Text style={styles.description}>
+                        Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+                    </Text>
+            </ImageBackground>
+
+            <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+                <Text style={styles.okButtonText}>
+                    Tudo bem
+                </Text>
+            </RectButton>
+        </View>
+    )
 }
 
 export default GiveClasses;
